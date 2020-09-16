4202e4cea0c1574a6ff076cd98149010ddbec471
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-16 01:08:22 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -88,8 +88,8 @@ connection {
    # private_key = ${var.jenkins_ssh}
     #  private_key = var.private_key_path
 private_key = file("~/.ssh/id_rsa")
-   # host        = aws_instance.example.public_ip
-  host            = self.ipv4_address
+    host        = aws_instance.example.public_ip
+ # host            = self.ipv4_address
        }
     }
   #  connection {
@@ -119,6 +119,6 @@ private_key = file("~/.ssh/id_rsa")
   # command = "ansible-playbook ANSIBLE_HOST_KEY_CHECKING=False -u ubuntu -i '${aws_instance.example.public_dns},' --private-key ${tls_private_key.example.private_key_pem} site.yml"
   # command = "ansible-playbook ANSIBLE_HOST_KEY_CHECKING=False -u ubuntu --prviate-key ${var.jenkins_ssh} -i '${aws_instance.example.public_dns},' site.yml"
    #command = "ansible-playbook --ssh-common-args= '-o StrictHostKeyChecking=no' -u ubuntu --prviate-key $(/Users/saziyamukadam/Downloads/jenkinskey.pem) -i '${aws_instance.example.public_dns},' site.yml"
-    command ="ansible-playbook -u ubuntu --private-key ${var.private_key_path} site.yml -i ${self.ipv4_address},"
+    command ="ansible-playbook -u ubuntu --private-key ${var.private_key_path} site.yml -i ${aws_instance.example.public_dns},"
      }
   }
