6cead8a4a0984ae0aaa3c28358683a132e49881b
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-16 01:06:09 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -87,7 +87,7 @@ connection {
     user        = "ubuntu"
    # private_key = ${var.jenkins_ssh}
     #  private_key = var.private_key_path
-private_key = file('~/.ssh/id_rsa')
+private_key = file("~/.ssh/id_rsa")
    # host        = aws_instance.example.public_ip
   host            = self.ipv4_address
        }
