1dfd774c511efe70e29a64d1a1a6601337f60439
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-09-29 01:26:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,9 @@ variable "public_key_path" {
   default     = ""
 }
 
-variable "private_key_path" {
+variable "private_key_file" {
   description = "Path to the private SSH key, used to access the instance."
-  default     = "/var/lib/jenkins/.ssh/jenkinskey"
+  default     = "~/.ssh/jenkinskey"
 }
 
 #variable "project_name" {
