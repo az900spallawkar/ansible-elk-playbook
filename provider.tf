458db0fc0d6e25e1ae638248b8c13e18e4c0082f
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-10-02 01:59:45 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -91,7 +91,7 @@ resource "aws_instance" "example" {
    # private_key = ${var.jenkins_ssh}
     #private_key = file("/var/lib/jenkins/jenkinskey")
   # private_key = var.private_key_file
-   private_key = file(var.private_key_file)
+   private_key = file("${var.private_key_file}")
     host        = aws_instance.example.public_ip
   
  # host            = self.ipv4_address
