6d82bc7add55cef1ce329dc8882ce65596cf4c00
az900spallawkar <66231134+az900spallawkar@users.noreply.github.com>
2020-10-03 21:51:36 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -128,8 +128,8 @@ resource "aws_instance" "example" {
   # command = "ansible-playbook ANSIBLE_HOST_KEY_CHECKING=False -u ubuntu --prviate-key ${var.jenkins_ssh} -i '${aws_instance.example.public_dns},' site.yml"
   # command = "ansible-playbook -u ubuntu --private-key $(var.private_key_path) -i '${aws_instance.example.public_dns},' site.yml"
     #command ="ansible-playbook -u ubuntu --private-key ${var.private_key_file} -i ${aws_instance.example.public_dns}, site.yml"
-     command ="ansible-playbook -u ubuntu site.yml -i ${aws_instance.example.public_dns},"
-    # command ="ansible-playbook -u ubuntu --key-file /var/lib/jenkins/jenkinskey.pem -i ${aws_instance.example.public_dns}, site.yml"
+    # command ="ansible-playbook -u ubuntu site.yml -i ${aws_instance.example.public_dns},"
+     command ="ansible-playbook -u ubuntu --key-file /var/lib/jenkins/.ssh/jenkinskey -i ${aws_instance.example.public_dns}, site.yml"
      connection {
     host = aws_instance.example.public_ip
     type        = "ssh"
